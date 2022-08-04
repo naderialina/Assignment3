3848047b38a8b65d17f163248b3d0c44fe328a15
naderialina <102945669+naderialina@users.noreply.github.com>
2022-08-03 22:10:54 -0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,40 @@
+<?php 
+
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+ ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>HOME</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+
+     <h1>Welcome! <?php echo $_SESSION['name']; ?></h1>
+
+
+</body>
+
+</html>
+
+<?php 
+
+}else{
+
+     header("Location: index.php");
+
+     exit();
+
+}
+
+ ?>
